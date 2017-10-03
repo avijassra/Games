62b9e2035badf6ba901887ea33a983b969e60346
Avi Jassra <avijassra@hotmail.com>
2017-10-03 15:33:24 -0400

    click events working

--- app/components/tic-tac-toe/tic-tac-toe.componet.js
+++ app/components/tic-tac-toe/tic-tac-toe.componet.js
@@ -5,18 +5,34 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
 var TicTacToeComponent = /** @class */ (function () {
     function TicTacToeComponent() {
+        this.isPlayer1 = true;
+        this.grid = [];
+        for (var i = 0; i < 3; i++) {
+            this.grid[i] = [];
+        }
     }
+    TicTacToeComponent.prototype.ticOrTac = function (row, col) {
+        this.grid[row][col] = (this.isPlayer1 ? 1 : 0);
+        this.isPlayer1 = !this.isPlayer1;
+    };
+    TicTacToeComponent.prototype.getCssClass = function (row, col) {
+        return ((this.isPlayer1 && (this.grid[row][col] === 1 || this.grid[row][col] === 0)) ? "fa-times" : "fa-circle-o");
+    };
     TicTacToeComponent = __decorate([
         core_1.Component({
             moduleId: module.id,
             selector: 'parent-di',
             templateUrl: 'tic-tac-toe.component.html',
-            styles: ["  \n                .ttt-g table { margin: 0px auto; float: none; }\n                .ttt-g table tr td { width: 150px !important; height: 150px !important; border: 2px solid #000; }\n                .ttt-g table tr td:first-child { border-left: none;}\n                .ttt-g table tr:first-child td { border-top: none;}\n                .ttt-g table tr td:last-child { border-right: none;}\n                .ttt-g table tr:last-child td { border-bottom: none;}"]
-        })
+            styles: ["  \n                .ttt-g table { margin: 0px auto; float: none; }\n                .ttt-g table tr td { width: 100px !important; height: 100px !important; border: 3px solid #000; }\n                .ttt-g table tr td:first-child { border-left: none;}\n                .ttt-g table tr:first-child td { border-top: none;}\n                .ttt-g table tr td:last-child { border-right: none;}\n                .ttt-g table tr:last-child td { border-bottom: none;}"]
+        }),
+        __metadata("design:paramtypes", [])
     ], TicTacToeComponent);
     return TicTacToeComponent;
 }());
--- app/components/tic-tac-toe/tic-tac-toe.componet.ts
+++ app/components/tic-tac-toe/tic-tac-toe.componet.ts
@@ -6,12 +6,29 @@ import { Component } from '@angular/core';
     templateUrl: 'tic-tac-toe.component.html',
     styles: [`  
                 .ttt-g table { margin: 0px auto; float: none; }
-                .ttt-g table tr td { width: 150px !important; height: 150px !important; border: 2px solid #000; }
+                .ttt-g table tr td { width: 100px !important; height: 100px !important; border: 3px solid #000; }
                 .ttt-g table tr td:first-child { border-left: none;}
                 .ttt-g table tr:first-child td { border-top: none;}
                 .ttt-g table tr td:last-child { border-right: none;}
                 .ttt-g table tr:last-child td { border-bottom: none;}`]
 })
 export class TicTacToeComponent {
+    isPlayer1 = true;
+    grid: number[][];
 
+    constructor() {
+        this.grid = [];
+        for(let i = 0; i < 3; i++) {
+            this.grid[i] = [];
+        }
+    }
+
+    ticOrTac(row:number, col:number): void {
+        this.grid[row][col] = (this.isPlayer1 ? 1 : 0)
+        this.isPlayer1 = !this.isPlayer1;
+    }
+
+    getCssClass(row:number, col: number): string {
+        return ((this.isPlayer1 && (this.grid[row][col] === 1 || this.grid[row][col] === 0)) ? "fa-times" : "fa-circle-o");
+    }
 }
