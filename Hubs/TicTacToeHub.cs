098451ac3463276d83045da2293762ad66e02561
Avi Jassra <avijassra@hotmail.com>
2017-10-06 15:35:56 -0400

    not working .... trying to resolve aspnet and signalR in angular import

--- Hubs/TicTacToeHub.cs
+++ Hubs/TicTacToeHub.cs
@@ -15,3 +15,5 @@ namespace Games.Hubs
 
 //https://blogs.msdn.microsoft.com/webdev/2017/09/14/announcing-signalr-for-asp-net-core-2-0/
 //https://stackoverflow.com/questions/31131490/how-to-subscribe-to-an-event-on-a-service-in-angular2
+//http://www.c-sharpcorner.com/article/asp-net-signalr-angular2-and-typescript-real-time-clock/
+//https://damienbod.com/2017/09/12/getting-started-with-signalr-using-asp-net-core-and-angular/
